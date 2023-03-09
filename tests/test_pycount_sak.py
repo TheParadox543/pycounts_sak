8af3f037ac11d18361b095e3004a18516a678b40
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2023-03-09 07:42:56 +0530

    test: create more tests
    Learned how to make unit tests, integration tests and regression tests

--- tests/test_pycount_sak.py
+++ tests/test_pycount_sak.py
@@ -4,6 +4,7 @@ from matplotlib.container import BarContainer
 from pycount_sak.datasets import get_flatland
 from pycount_sak.plotting import plot_words
 from pycount_sak.pycount_sak import count_words
+from pytest import raises
 
 def test_count_words():
     """Test word counting from a file."""
@@ -14,18 +15,18 @@ def test_count_words():
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
 
-def test_count_flatland():
-    """Test flatland function."""
-    expected = Counter({'to': 3, 'call': 2, 'i': 1, 'our': 1,
-                        'world': 1, 'flatland': 1, 'not': 1,
-                        'because': 1, 'we': 1, 'it': 1, 'so': 1,
-                        'but': 1, 'make': 1, 'its': 1, 'nature': 1,
-                        'clearer': 1, 'you': 1, 'my': 1, 'happy': 1,
-                        'readers': 1, 'who': 1, 'are': 1,
-                        'privileged': 1, 'live': 1, 'in': 1,
-                        'space': 1})
-    actual = count_words(get_flatland())
-    assert actual == expected, "Flatland quote counted incorrectly!"
+# def test_count_flatland():
+#     """Test flatland function."""
+#     expected = Counter({'to': 3, 'call': 2, 'i': 1, 'our': 1,
+#                         'world': 1, 'flatland': 1, 'not': 1,
+#                         'because': 1, 'we': 1, 'it': 1, 'so': 1,
+#                         'but': 1, 'make': 1, 'its': 1, 'nature': 1,
+#                         'clearer': 1, 'you': 1, 'my': 1, 'happy': 1,
+#                         'readers': 1, 'who': 1, 'are': 1,
+#                         'privileged': 1, 'live': 1, 'in': 1,
+#                         'space': 1})
+#     actual = count_words(get_flatland())
+#     assert actual == expected, "Flatland quote counted incorrectly!"
 
 def test_plot_words():
     """Test plotting of word counts."""
@@ -36,3 +37,23 @@ def test_plot_words():
     fig = plot_words(counts)
     assert isinstance(fig, BarContainer), "Wrong plot type"
     assert len(fig.datavalues) == 10, "Incorrect number of bars plotted"
+
+def test_plot_words_error():
+    """Check TypeError raised when Counter not used."""
+    with raises(TypeError):
+        list_object = ["Pythons", "are", "not", "venomous"]
+        plot_words(list_object)
+
+def test_integration():
+    """Test count_words() and plot_words workflow."""
+    counts = count_words("tests/einstein.txt")
+    fig = plot_words(counts)
+    assert isinstance(fig, BarContainer), "Wrong plot type"
+    assert len(fig.datavalues) == 10, "Incorrect number of bars plotted"
+    assert max(fig.datavalues) == 2, "Highest word count should be 2"
+
+def test_regression():
+    """Regression test for Flatland."""
+    top_word = count_words(get_flatland()).most_common(1)
+    assert top_word[0][0] == "the", "Most common word is not 'the'"
+    assert top_word[0][1] == 2276, "'the' count has changed"
